388c12cc9804ca618be89171fc6d237dae832978
tangerine238 <stephentang238@gmail.com>
2026-04-10 20:42:15 -0400

    First Push

---
+++ Source/FreezeTag/FreezeTag.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FreezeTag : ModuleRules
+{
+	public FreezeTag(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"FreezeTag",
+			"FreezeTag/Variant_Platforming",
+			"FreezeTag/Variant_Platforming/Animation",
+			"FreezeTag/Variant_Combat",
+			"FreezeTag/Variant_Combat/AI",
+			"FreezeTag/Variant_Combat/Animation",
+			"FreezeTag/Variant_Combat/Gameplay",
+			"FreezeTag/Variant_Combat/Interfaces",
+			"FreezeTag/Variant_Combat/UI",
+			"FreezeTag/Variant_SideScrolling",
+			"FreezeTag/Variant_SideScrolling/AI",
+			"FreezeTag/Variant_SideScrolling/Gameplay",
+			"FreezeTag/Variant_SideScrolling/Interfaces",
+			"FreezeTag/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/FreezeTag/FreezeTag.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FreezeTag.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FreezeTag, "FreezeTag" );
+
+DEFINE_LOG_CATEGORY(LogFreezeTag)
